d80e22f03a53a819e1915dc7571862d7782955eb
Roma Kutischev <rukkola2@gmail.com>
2020-06-29 21:05:18 +0300

    Loader and error message

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { useEffect } from 'react';
+import { Container, Dimmer, Loader, Message, Sidebar, Segment } from "semantic-ui-react";
 import { useSelector, useDispatch } from "react-redux";
 
+import Calendar from "./components/Calendar";
+import SidebarComponent from "./components/SidebarComponent";
 import { eventsLoadStart } from "./store/actions/events"
 import './App.css';
 
 function App() {
+  const events = useSelector(state => state.events);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(eventsLoadStart());
-  }, [eventsLoadStart, dispatch]);
+  }, [dispatch]);
 
   return (
-    <div className="App">
+    <Sidebar.Pushable as={Segment}>
+      <SidebarComponent />
 
-    </div>
+      <Sidebar.Pusher as={Container} className="application">
+        {events.loadingState === "loading" &&
+          <Dimmer active>
+            <Loader>Loading</Loader>
+          </Dimmer>}
+        {events.loadingState === "failed" &&
+          <Message negative>
+            <Message.Header>Looks like, something went wrong</Message.Header>
+            <p>{events.error}</p>
+          </Message>}
+        {events.loadingState === "succeed" && <Calendar />}
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
+
   );
 }
 
---
+++ src/components/Calendar.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+function Calendar() {
+  
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default Calendar;
